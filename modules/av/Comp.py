e6a2bdf09fc9e351f3157b3425d52f22383e8c55
M. Oostrik <m.oostrik@gmail.com>
2025-09-09 11:58:39 +0200

    workin' on animation

--- modules/av/Comp.py
+++ modules/av/Comp.py
@@ -1,3 +1,7 @@
+from turtle import width
+import wave
+from cv2 import threshold
+from numpy._typing._array_like import NDArray
 from modules.av.Definitions import *
 from typing import Optional
 import math
@@ -17,19 +21,19 @@ from modules.utils.HotReloadMethods import HotReloadMethods
 @dataclass
 class TestParameters():
     void_width: float =  0.05
-    
-    
+
+
     pattern_width: float = 0.2
     pattern_sharpness: float = 1.5
-    
-    
+
+
     pattern_edge: float = 0.2
     input_A0: float = 0.0
     input_A1: float = 0.0
     input_B0: float = 0.0
     input_B1: float = 0.0
     show_void: bool = False
-    
+
     interval: float = 0.1
     num_players: int = 1
 
@@ -39,7 +43,7 @@ class Comp():
         self.parameters: TestParameters = TestParameters(
             interval=1.0 / settings.light_rate,
             num_players=settings.max_players)
-        
+
         # self.draw_methods: DrawMethods = DrawMethods()
         self.pose_metrics: MultiPoseMetrics = MultiPoseMetrics(settings)
 
@@ -49,8 +53,8 @@ class Comp():
         self.output_img: np.ndarray = np.zeros((1, self.resolution, 3), dtype=IMG_TYPE)
 
         self.hot_reloader = HotReloadMethods(self.__class__, True)
-      
-      
+
+
     # SETTERS
     def set_smoothness(self, value: float) -> None:
         self.pose_metrics.set_smoothness(value)
@@ -70,24 +74,25 @@ class Comp():
         self.parameters.input_B1 = max(0.0, min(1.0, value))
     def show_overlay(self, value: bool) -> None:
         self.parameters.show_void = value
-        
+
 
     def update(self, poses: list[Pose]) -> np.ndarray:
-        
-        self.pose_metrics.update(poses)
+
+        self.pose_metrics.add_poses(poses)
+        self.pose_metrics.update()
 
         self.blue_array.fill(0.0)
         self.white_array.fill(1.0)
         self.make_voids(self.void_array, self.pose_metrics, self.parameters)
-        self.make_patterns(self.white_array, self.pose_metrics, self.parameters)
+        self.make_patterns(self.white_array, self.blue_array, self.pose_metrics, self.parameters)
 
-        # add overlay to second channel 
+        # add overlay to second channel
         if self.parameters.show_void:
             inverted_void_array = 1.0 - self.void_array
-            Comp.blend_values(self.white_array, inverted_void_array, 0, BlendType.MULTIPLY)  
+            Comp.blend_values(self.white_array, inverted_void_array, 0, BlendType.MULTIPLY)
             Comp.blend_values(self.blue_array, inverted_void_array, 0, BlendType.MULTIPLY)
             Comp.blend_values(self.blue_array, self.void_array * 0.5, 0, BlendType.ADD)
-            
+
         self.output_img[0, :, 0] = self.white_array[:]
         self.output_img[0, :, 1] = self.blue_array[:]
 
@@ -102,79 +107,132 @@ class Comp():
         array -= P.interval * 4.0
         np.clip(array, 0, 1, out=array)
 
-        world_positions: dict[int, float] = pose_metrics.world_positions
-        ages: dict[int, float] = pose_metrics.ages
-        pose_lengths: dict[int, float] = pose_metrics.pose_lengths
-        
+        world_positions: dict[int, float | None] = pose_metrics.world_positions
+        ages: dict[int, float | None] = pose_metrics.ages
+        pose_lengths: dict[int, float | None] = pose_metrics.pose_lengths
+
         for i in range(len(world_positions)):
-            if world_positions[i] is not None and pose_lengths[i] is not None and ages[i] is not None:
-                age = pow(min(ages[i] * 1.8, 1.0), 1.5)
+            centre: float | None = world_positions[i]
+            length: float | None = pose_lengths[i]
+            age: float | None = ages[i]
+
+            if centre is not None and length is not None and age is not None:
+                strength: float = pow(min(age * 1.8, 1.0), 1.5)
                 void_width: float = P.void_width * 0.036
-                width = (void_width + pose_lengths[i] * void_width)
-                Comp.draw_field(array, world_positions[i], width, age, 20, BlendType.MAX)
+                width: float = (void_width + length * void_width)
+                edge: float = 20
+                Comp.draw_field(array, centre, width, strength, edge, BlendType.MAX)
 
 
     @staticmethod
-    def make_patterns(array: np.ndarray, pose_metrics: MultiPoseMetrics, P: TestParameters) -> None:
-        array.fill(0.0)
+    def make_patterns(whites: np.ndarray, blues: np.ndarray, pose_metrics: MultiPoseMetrics, P: TestParameters) -> None:
+        whites.fill(0.0)
+        blues.fill(0.0)
 
-        world_positions: dict[int, float] = pose_metrics.world_positions
-        ages: dict[int, float] = pose_metrics.ages
-        pose_lengths: dict[int, float] = pose_metrics.pose_lengths
-        
-        for i in range(len(world_positions)):
-            if world_positions[i] is not None and pose_lengths[i] is not None and ages[i] is not None:
-                age = pow(min(ages[i] * 1.8, 1.0), 1.5)
-                width: float = P.pattern_width 
-                # width = (pattern_width + pose_lengths[i] * pattern_width)
-                Comp.draw_lines(array, world_positions[i], width, 90, ages[i], BlendType.ADD)
-                
+        world_positions: dict[int, float | None] = pose_metrics.world_positions
+        ages: dict[int, float | None] = pose_metrics.ages
+        pose_lengths: dict[int, float | None] = pose_metrics.pose_lengths
+        num_player_width: float = 1.0 / max(pose_metrics.smooth_num_active_players, 1)
+        pattern_width: float = P.pattern_width * 0.25 + num_player_width * P.pattern_width * 0.25
+
+        for i in range(pose_metrics.num_players):
+
+            if pose_metrics.is_player_present(i) and world_positions[i] is not None:
+                centre: float | None = world_positions[i]
+                age: float | None = ages[i]
+                if centre is None or age is None:
+                    continue
+                age_pattern_speed: float = 0.25
+                age_pattern_power: float = 0.75
+                age_pattern_width: float = pattern_width * pow(min(age * age_pattern_speed, 1.0), age_pattern_power)
+
+                white_line_count_A: float = 44  #+ (P.input_A0 * 88) + (P.input_A1 * 88)
+                white_line_count_B: float = 22  #+ (P.input_B0 * 88) + (P.input_B1 * 88)
+                white_line_width_A: float = 0.3 #+ (P.input_A0 * 0.7)
+                white_line_width_B: float = 0.3 #+ (P.input_B0 * 0.7)
+                white_line_speed_A: float = 2 #+ (P.input_A1 * 13.2)
+                white_line_speed_B: float = 2.2 #+ (P.input_B1 * 13.2)
+
+                blend: BlendType = BlendType.MAX
+
+                Comp.draw_lines(whites, centre, age_pattern_width, white_line_count_A, white_line_width_A, white_line_speed_A, blend, False)
+                Comp.draw_lines(whites, centre, -age_pattern_width, white_line_count_A, white_line_width_A, -white_line_speed_A, blend, False)
+                # Comp.draw_lines(whites, centre, age_pattern_width, white_line_count_B, white_line_width_B, white_line_speed_B, blend, False)
+                # Comp.draw_lines(whites, centre, -age_pattern_width, white_line_count_B, white_line_width_B, -white_line_speed_B, blend, False)
+
+
+                # Comp.draw_lines(blues, centre, age_pattern_width, white_line_count_A, white_line_width_A, -white_line_speed_A, blend, False)
+                # Comp.draw_lines(blues, centre, -age_pattern_width, white_line_count_A, white_line_width_A, white_line_speed_A, blend, False)
+                # Comp.draw_lines(blues, centre, age_pattern_width, white_line_count_B, white_line_width_B, -white_line_speed_B, blend, False)
+                # Comp.draw_lines(blues, centre, -age_pattern_width, white_line_count_B, white_line_width_B, white_line_speed_B, blend, False)
+
+
+                # blue_line_count: float = 44 + (P.input_A0 * 88) + (P.input_A1 * 88)
+                # blue_line_width: float = 0.3 + (P.input_A0 * 0.7)
+                # blue_line_speed: float = 6.6 + (P.input_A1 * 13.2)
+
+                # Comp.draw_lines(blues, centre, age_pattern_width, blue_line_count, blue_line_width, -blue_line_speed, BlendType.ADD)
+                # Comp.draw_lines(blues, centre, -age_pattern_width, blue_line_count, blue_line_width, blue_line_speed, BlendType.ADD)
+
 
     @staticmethod
-    def draw_lines(array: np.ndarray, centre: float, width: float, line_count: float, age: float, blend: BlendType) -> None:
+    def draw_lines(array: np.ndarray, centre: float, width: float, line_count: float, line_width: float, speed: float, blend: BlendType, relative_to_world: bool = False) -> None:
         resolution: int = len(array)
         field_centre: int = int(centre * resolution)
         field_width: int = int(width * resolution)
-        idx_start = int((field_centre - field_width // 2) % resolution)
-        
+        idx_start: int = field_centre if width >= 0 else (field_centre + field_width)
+        field_width = abs(field_width)
+
         world_positions = (np.arange(field_width) + idx_start) % resolution
         world_positions = world_positions / resolution  # Normalize to 0.0-1.0 range
 
-        # Use world positions to create wave pattern
-        time_offset = age * 10.0
-        wave_phase_per_unit = 2 * math.pi * line_count
-        phases = world_positions * wave_phase_per_unit - time_offset
-        values = (0.5 * np.sin(phases) + 0.5)
+        time_offset: float = time.time() * speed
+        wave_phase_per_unit: float = 2 * math.pi * line_count
 
-        # sharpness = Comp.pattern_sharpness
-        sharpness = 2.5
-        values **= sharpness
+        if relative_to_world:
+            phases = world_positions * wave_phase_per_unit - time_offset
+        else:
+            phase_shift: float = math.pi if speed < 0 else 0.0
+            rel_positions = world_positions - centre
+            phases = rel_positions * wave_phase_per_unit - time_offset + phase_shift
+
+        values = (1.0 + np.sin(phases)) * 0.5
+
+        if line_width == 0.0:
+            return
+        if line_width == 1.0:
+            values.fill(1.0)
+        else:
+            power_factor: float = 1.0 / line_width - 1.0
+            values: np.ndarray = np.power(values, power_factor)
+
 
         Comp.draw_edge(values, int(field_width * 0.1), 1.5)
-        
         Comp.apply_circular(array, values, idx_start, blend)
-        
-        
-    @staticmethod 
-    def draw_field(array: np.ndarray, centre: float, width: float, value: float, edge: int, blend: BlendType) -> None:
+
+
+    @staticmethod
+    def draw_field(array: np.ndarray, centre: float, width: float, strength: float, edge: int, blend: BlendType) -> None:
         resolution: int = len(array)
         field_centre: int = int(centre * resolution)
         field_width: int = int(width * resolution)
         idx_start = int((field_centre - field_width // 2) % resolution)
-        
-        values = np.full(field_width, value, dtype=array.dtype)
-        
+
+        values = np.full(field_width, strength, dtype=array.dtype)
+
         edge_width: int = int(min(edge, field_width // 2))
         if edge_width > 0:
             Comp.draw_edge(values, edge_width, 1.5)
-        
+
         Comp.apply_circular(array, values, idx_start, blend)
-        
+
     @staticmethod
     def draw_edge(array: np.ndarray, edge: int, curve: float) -> None:
         resolution: int = len(array)
         if edge > resolution:
             edge = resolution
+        if edge == 0:
+            return
 
         array[:edge] *= np.linspace(0, 1, edge) ** curve
         array[-edge:] *= np.linspace(1, 0, edge) ** curve
@@ -183,10 +241,11 @@ class Comp():
     def apply_circular(array: np.ndarray, values: np.ndarray, start_idx: int, blend: BlendType) -> None:
         resolution: int = len(array)
         # print(array)
-        
+
+        start_idx = start_idx % resolution
         end_idx: int = (start_idx + len(values)) % resolution
         if start_idx < end_idx:
-           Comp.blend_values(array, values, start_idx, blend)
+            Comp.blend_values(array, values, start_idx, blend)
         else:
             Comp.blend_values(array, values[:resolution - start_idx], start_idx, blend)
             Comp.blend_values(array, values[resolution - start_idx:], 0, blend)
@@ -195,15 +254,15 @@ class Comp():
     def blend_values(array: np.ndarray, values: np.ndarray, start_idx: int, blend: BlendType) -> None:
         resolution: int = len(array)
         # print(blend)
-        
+
         end_idx = start_idx + len(values)
         if start_idx < 0:
             start_idx = 0
         if end_idx > resolution:
             end_idx = resolution
-            
+
             # raise ValueError("start_idx and end_idx must be within the bounds of the array.")
-        
+
         if start_idx < end_idx:
             if blend == BlendType.NONE:
                 array[start_idx:end_idx] =  values
@@ -220,5 +279,5 @@ class Comp():
             elif blend == BlendType.NON_ZERO:
                 mask = values != 0
                 array[start_idx:end_idx][mask] = values[mask]
-                
+
         np.clip(array, 0, 1, out=array)
--- modules/av/PoseMetrics.py
+++ modules/av/PoseMetrics.py
@@ -22,9 +22,9 @@ class PoseMetrics:
 
         # input_fps = settings.camera_fps
         output_fps = settings.light_rate
-        
+
         self.filters: dict[str, SmoothOneEuro | SmoothOneEuroCircular] = {}
-        
+
         self.filters["world_angle"] = SmoothOneEuroCircular(freq = output_fps)
         self.filters["approximate_person_length"] = SmoothOneEuro(freq = output_fps)
         for key in PoseAngleKeypoints.keys():
@@ -35,7 +35,7 @@ class PoseMetrics:
         self.age: float | None = 0.0
 
         self.hot_reloader = HotReloadMethods(self.__class__, True)
-            
+
     def __getattr__(self, name):
         """Allow access to smoothed values as attributes."""
         if "filters" in self.__dict__ and name in self.filters:
@@ -43,7 +43,7 @@ class PoseMetrics:
             value = smoother.get_smoothed_value()
             return value
         raise AttributeError(f"'SmoothMetrics' object has no attribute '{name}'")
-        
+
     def __setattr__(self, name, value):
         # Avoid recursion for internal attributes
         if "filters" in self.__dict__ and name in self.filters:
@@ -51,11 +51,11 @@ class PoseMetrics:
             filter.add_value(value)
         else:
             super().__setattr__(name, value)
-        
+
     def update(self) -> None:
         for filter in self.filters.values():
             filter.update()
-            
+
         self.age = time() - self.start_age if self.start_age is not None else None
 
     def add_pose(self, pose: Pose) -> None:
@@ -63,11 +63,11 @@ class PoseMetrics:
         if tracklet is not None and tracklet.is_active and tracklet.age_in_seconds > 2.0:
             if self.start_age is None:
                 self.start_age = time()
-        
+
             angles: JointAngleDict | None  = pose.angles
             if angles is not None:
                 # print(angles)
-                for key in PoseAngleKeypoints.keys():                
+                for key in PoseAngleKeypoints.keys():
                     angle_value = angles[key]
                     angle_name = key.name
                     if angle_value['confidence'] > 0.3 and angle_value['angle'] is not np.nan:
@@ -76,7 +76,7 @@ class PoseMetrics:
                     # print(f"Setting {angle_name} to {angle_value.angle}")
                     # setattr(self, angle_name, angle_value)
 
-            approximate_person_length: float = pose.get_approximate_person_length()
+            approximate_person_length: float | None = pose.get_approximate_person_length()
             if approximate_person_length is not None:
                 self.approximate_person_length = approximate_person_length #* min(self.age, 1.0)
 
@@ -87,67 +87,89 @@ class PoseMetrics:
 
         if pose.is_final:
             self.reset()
-            
+
     def reset(self) -> None:
         """Reset all smoothers"""
         for smoother in self.filters.values():
-            smoother.reset()  
+            smoother.reset()
         self.age = None
         self.start_age = None
 
     def set_smoothness(self, value: float) -> None:
         for smoother in self.filters.values():
             smoother.set_smoothness(value)
-            
+
     def set_responsiveness(self, value: float) -> None:
         for smoother in self.filters.values():
             smoother.set_responsiveness(value)
-            
-            
-            
-class MultiPoseMetrics:    
-    def __init__(self, settings: Settings):        
+
+
+
+class MultiPoseMetrics:
+    def __init__(self, settings: Settings):
         self.num_players: int = settings.max_players
-        
+
         self.smooth_metrics_dict: dict[int, PoseMetrics] = {}
         for i in range(self.num_players):
             self.smooth_metrics_dict[i] = PoseMetrics(settings)
-            
-        self.world_positions: dict[int, float] = {}
-        self.pose_lengths: dict[int, float] = {}        
-        self.ages: dict[int, float] = {}
-        self.left_shoulders: dict[int, float] = {}        
-        self.right_shoulders: dict[int, float] = {}
-        self.left_elbows: dict[int, float] = {}
-        self.right_elbows: dict[int, float] = {}
+
+        self.num_active_players_smoother: SmoothOneEuro = SmoothOneEuro(freq = settings.light_rate)
+
+        self.world_positions: dict[int, float | None] = {}
+        self.pose_lengths: dict[int, float | None] = {}
+        self.ages: dict[int, float | None] = {}
+        self.left_shoulders: dict[int, float | None] = {}
+        self.right_shoulders: dict[int, float | None] = {}
+        self.left_elbows: dict[int, float | None] = {}
+        self.right_elbows: dict[int, float | None] = {}
 
         self.hot_reloader = HotReloadMethods(self.__class__, True)
-        
 
+    @ property
+    def num_active_players(self) -> int:
+        count: int = 0
+        for i in range(self.num_players):
+            count += self.is_player_present(i)
+        return count
 
-    def update(self, poses: list[Pose]) -> None:
+    @property
+    def smooth_num_active_players(self) -> float:
+        smoothed_value: float | None = self.num_active_players_smoother.get_smoothed_value()
+        if smoothed_value is None:
+            return 0.0
+        return smoothed_value
+
+    def add_poses(self, poses: list[Pose]) -> None:
         for pose in poses:
             self.smooth_metrics_dict[pose.id].add_pose(pose)
-            
+
+    def update(self) -> None:
         for key, sm in self.smooth_metrics_dict.items():
             sm.update()
             self.world_positions[key] = sm.world_angle
-            self.pose_lengths[key] = sm.approximate_person_length            
+            self.pose_lengths[key] = sm.approximate_person_length
             self.ages[key] = sm.age
             self.left_shoulders[key] = sm.left_shoulder
             self.right_shoulders[key] = sm.right_shoulder
             self.left_elbows[key] = sm.left_elbow
             self.right_elbows[key] = sm.right_elbow
-            
+
+        self.num_active_players_smoother.add_value(self.num_active_players)
+        self.num_active_players_smoother.update()
+
     def set_smoothness(self, value: float) -> None:
         for sm in self.smooth_metrics_dict.values():
             sm.set_smoothness(value)
-            
+
     def set_responsiveness(self, value: float) -> None:
         for sm in self.smooth_metrics_dict.values():
             sm.set_responsiveness(value)
-            
+
     def reset(self) -> None:
         for sm in self.smooth_metrics_dict.values():
             sm.reset()
+        self.update()
+
+    def is_player_present(self, index: int) -> bool:
+        return self.world_positions.get(index, None) is not None
 
