33b29dcac6cd39d80dad0f519037edb70d3b488d
M. Oostrik <m.oostrik@gmail.com>
2025-09-27 15:29:54 +0200

    cleanup

--- modules/pose/PoseTypes.py
+++ modules/pose/PoseTypes.py
@@ -42,17 +42,17 @@ class PoseJoint(IntEnum):
     left_ankle =    15
     right_ankle =   16
 POSE_JOINT_NAMES: list[str] = [e.name for e in PoseJoint]
-POSE_NUM_JOINTS: int = len(PoseJoint)
+POSE_NUM_JOINTS:  int = len(PoseJoint)
 
 # COLORS
-POSE_COLOR_ALPHA_BASE: float = 0.2
-POSE_COLOR_CENTER: tuple[float, float, float] = (0.8, 0.8, 0.8)   # Light Gray
-POSE_COLOR_LEFT: tuple[float, float, float] = (1.0, 0.5, 0.0)     # Orange
-POSE_COLOR_LEFT_POSITIVE: tuple[float, float, float] = (1.0, 1.0, 0.0)  # Yellow
-POSE_COLOR_LEFT_NEGATIVE: tuple[float, float, float] = (1.0, 0.0, 0.0)  # Red
-POSE_COLOR_RIGHT: tuple[float, float, float] = (0.0, 1.0, 1.0)    # Cyan
-POSE_COLOR_RIGHT_POSITIVE: tuple[float, float, float] = (0.0, 0.5, 1.0) # Light Blue
-POSE_COLOR_RIGHT_NEGATIVE: tuple[float, float, float] = (0.0, 1.0, 0.5) # Light Green
+POSE_COLOR_ALPHA_BASE:      float = 0.2
+POSE_COLOR_CENTER:          tuple[float, float, float] = (0.8, 0.8, 0.8)   # Light Gray
+POSE_COLOR_LEFT:            tuple[float, float, float] = (1.0, 0.5, 0.0)     # Orange
+POSE_COLOR_LEFT_POSITIVE:   tuple[float, float, float] = (1.0, 1.0, 0.0)  # Yellow
+POSE_COLOR_LEFT_NEGATIVE:   tuple[float, float, float] = (1.0, 0.0, 0.0)  # Red
+POSE_COLOR_RIGHT:           tuple[float, float, float] = (0.0, 1.0, 1.0)    # Cyan
+POSE_COLOR_RIGHT_POSITIVE:  tuple[float, float, float] = (0.0, 0.5, 1.0) # Light Blue
+POSE_COLOR_RIGHT_NEGATIVE:  tuple[float, float, float] = (0.0, 1.0, 0.5) # Light Green
 
 
 # Define color for each joint
@@ -61,43 +61,43 @@ POSE_JOINT_COLORS: dict[PoseJoint, tuple[float, float, float]] = {
     PoseJoint.nose: POSE_COLOR_CENTER,
 
     # Left side points - orange
-    PoseJoint.left_eye: POSE_COLOR_LEFT,
-    PoseJoint.left_ear: POSE_COLOR_LEFT,
-    PoseJoint.left_shoulder: POSE_COLOR_LEFT,
-    PoseJoint.left_elbow: POSE_COLOR_LEFT,
-    PoseJoint.left_wrist: POSE_COLOR_LEFT,
-    PoseJoint.left_hip: POSE_COLOR_LEFT,
-    PoseJoint.left_knee: POSE_COLOR_LEFT,
-    PoseJoint.left_ankle: POSE_COLOR_LEFT,
+    PoseJoint.left_eye:         POSE_COLOR_LEFT,
+    PoseJoint.left_ear:         POSE_COLOR_LEFT,
+    PoseJoint.left_shoulder:    POSE_COLOR_LEFT,
+    PoseJoint.left_elbow:       POSE_COLOR_LEFT,
+    PoseJoint.left_wrist:       POSE_COLOR_LEFT,
+    PoseJoint.left_hip:         POSE_COLOR_LEFT,
+    PoseJoint.left_knee:        POSE_COLOR_LEFT,
+    PoseJoint.left_ankle:       POSE_COLOR_LEFT,
 
     # Right side points - cyan
-    PoseJoint.right_eye: POSE_COLOR_RIGHT,
-    PoseJoint.right_ear: POSE_COLOR_RIGHT,
-    PoseJoint.right_shoulder: POSE_COLOR_RIGHT,
-    PoseJoint.right_elbow: POSE_COLOR_RIGHT,
-    PoseJoint.right_wrist: POSE_COLOR_RIGHT,
-    PoseJoint.right_hip: POSE_COLOR_RIGHT,
-    PoseJoint.right_knee: POSE_COLOR_RIGHT,
-    PoseJoint.right_ankle: POSE_COLOR_RIGHT
+    PoseJoint.right_eye:        POSE_COLOR_RIGHT,
+    PoseJoint.right_ear:        POSE_COLOR_RIGHT,
+    PoseJoint.right_shoulder:   POSE_COLOR_RIGHT,
+    PoseJoint.right_elbow:      POSE_COLOR_RIGHT,
+    PoseJoint.right_wrist:      POSE_COLOR_RIGHT,
+    PoseJoint.right_hip:        POSE_COLOR_RIGHT,
+    PoseJoint.right_knee:       POSE_COLOR_RIGHT,
+    PoseJoint.right_ankle:      POSE_COLOR_RIGHT
 }
 
 # VERTICES
 POSE_VERTEX_LIST: list[list[PoseJoint]] = [
-    [PoseJoint.nose, PoseJoint.left_eye],
-    [PoseJoint.nose, PoseJoint.right_eye],
-    [PoseJoint.left_eye, PoseJoint.left_ear],
-    [PoseJoint.right_eye, PoseJoint.right_ear],
-    [PoseJoint.left_shoulder, PoseJoint.right_shoulder],
-    [PoseJoint.left_shoulder, PoseJoint.left_elbow],
-    [PoseJoint.right_shoulder, PoseJoint.right_elbow],
-    [PoseJoint.left_elbow, PoseJoint.left_wrist],
-    [PoseJoint.right_elbow, PoseJoint.right_wrist],
-    [PoseJoint.left_shoulder, PoseJoint.left_hip],
-    [PoseJoint.right_shoulder, PoseJoint.right_hip],
-    [PoseJoint.left_hip, PoseJoint.left_knee],
-    [PoseJoint.right_hip, PoseJoint.right_knee],
-    [PoseJoint.left_knee, PoseJoint.left_ankle],
-    [PoseJoint.right_knee, PoseJoint.right_ankle]
+    [PoseJoint.nose,            PoseJoint.left_eye],
+    [PoseJoint.nose,            PoseJoint.right_eye],
+    [PoseJoint.left_eye,        PoseJoint.left_ear],
+    [PoseJoint.right_eye,       PoseJoint.right_ear],
+    [PoseJoint.left_shoulder,   PoseJoint.right_shoulder],
+    [PoseJoint.left_shoulder,   PoseJoint.left_elbow],
+    [PoseJoint.right_shoulder,  PoseJoint.right_elbow],
+    [PoseJoint.left_elbow,      PoseJoint.left_wrist],
+    [PoseJoint.right_elbow,     PoseJoint.right_wrist],
+    [PoseJoint.left_shoulder,   PoseJoint.left_hip],
+    [PoseJoint.right_shoulder,  PoseJoint.right_hip],
+    [PoseJoint.left_hip,        PoseJoint.left_knee],
+    [PoseJoint.right_hip,       PoseJoint.right_knee],
+    [PoseJoint.left_knee,       PoseJoint.left_ankle],
+    [PoseJoint.right_knee,      PoseJoint.right_ankle]
 ]
 POSE_VERTEX_ARRAY: np.ndarray = np.array([kp.value for pose in POSE_VERTEX_LIST for kp in pose], dtype=np.int32)
 POSE_VERTEX_INDICES: np.ndarray = np.arange(len(POSE_VERTEX_ARRAY), dtype=np.int32)
@@ -119,14 +119,14 @@ POSE_ANGLE_JOINT_IDXS: dict[PoseJoint, int] = {joint: idx for idx, joint in enum
 
 POSE_NUM_ANGLES: int = len(POSE_ANGLE_JOINT_TRIPLETS)
 POSE_ANGLE_ROTATIONS: dict[PoseJoint, float] = {
-    PoseJoint.left_shoulder:  0.0,
-    PoseJoint.right_shoulder: 0.0,
-    PoseJoint.left_elbow:     np.pi,
-    PoseJoint.right_elbow:    np.pi,
-    PoseJoint.left_hip:       np.pi,
-    PoseJoint.right_hip:      np.pi,
-    PoseJoint.left_knee:      np.pi,
-    PoseJoint.right_knee:     np.pi
+    PoseJoint.left_shoulder:    0.0,
+    PoseJoint.right_shoulder:   0.0,
+    PoseJoint.left_elbow:       np.pi,
+    PoseJoint.right_elbow:      np.pi,
+    PoseJoint.left_hip:         np.pi,
+    PoseJoint.right_hip:        np.pi,
+    PoseJoint.left_knee:        np.pi,
+    PoseJoint.right_knee:       np.pi
 }
 
 # ANATOMICAL PROPORTIONS
