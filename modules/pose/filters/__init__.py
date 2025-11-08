87f72686c2fb62fd1661ff3715821fdc1455cb53
M. Oostrik <m.oostrik@gmail.com>
2025-11-08 04:05:20 +0100

    Moved new filters to filter folder

--- modules/pose/filters/__init__.py
+++ modules/pose/filters/__init__.py
@@ -5,10 +5,10 @@ from .PoseFilterPipeline import                     PoseFilterPipeline
 from .PoseFilterTracker import                      PoseFilterTracker
 from .PoseFilterPipelineTracker import              PoseFilterPipelineTracker
 
-from .extractor.PoseAngleExtractor import           PoseAngleExtractor
-from .extractor.PoseDeltaExtractor import           PoseDeltaExtractor
-from .extractor.PoseSymmetryExtractor import        PoseSymmetryExtractor
-from .extractor.PoseMotionTimeAccumulator import    PoseMotionTimeAccumulator
+from .extractors.PoseAngleExtractor import          PoseAngleExtractor
+from .extractors.PoseDeltaExtractor import          PoseDeltaExtractor
+from .extractors.PoseSymmetryExtractor import       PoseSymmetryExtractor
+from .extractors.PoseMotionTimeAccumulator import   PoseMotionTimeAccumulator
 
 from .general.PoseValidators import                 PoseValidatorConfig, PoseNanValidator, PoseRangeValidator, PoseScoreValidator, PoseValidator
 
@@ -18,11 +18,12 @@ from .general.PosePredictors import                 PosePredictorConfig, PoseAng
 from .general.PoseSmoothers import                  PoseSmootherConfig, PoseAngleSmoother, PoseDeltaSmoother, PosePointSmoother, PoseSmoother
 from .general.PoseStickyFillers import              PoseStickyFillerConfig, PoseAngleStickyFiller, PoseDeltaStickyFiller, PosePointStickyFiller, PoseStickyFiller
 
+
 # Old implementations (deprecated, aliased to avoid breaking changes)
-from .smooth.PoseSmootherBase import                PoseSmootherConfig as OldPoseSmootherConfig
-from .smooth.PosePointSmoother import               PosePointSmoother as OldPosePointSmoother
-from .smooth.PoseAngleSmoother import               PoseAngleSmoother as OldPoseAngleSmoother
-from .smooth.PoseDeltaSmoother import               PoseDeltaSmoother as OldPoseDeltaSmoother
-from .smooth.PoseBBoxSmoother import                PoseBBoxSmoother
-from .interpolation.PoseInterpolatorConfig import   PoseInterpolatorConfig
-from .interpolation.PoseInterpolator import         PoseInterpolator
+from ..filter.smooth.PoseSmootherBase import                PoseSmootherConfig as OldPoseSmootherConfig
+from ..filter.smooth.PosePointSmoother import               PosePointSmoother as OldPosePointSmoother
+from ..filter.smooth.PoseAngleSmoother import               PoseAngleSmoother as OldPoseAngleSmoother
+from ..filter.smooth.PoseDeltaSmoother import               PoseDeltaSmoother as OldPoseDeltaSmoother
+from ..filter.smooth.PoseBBoxSmoother import                PoseBBoxSmoother
+from ..filter.interpolation.PoseInterpolatorConfig import   PoseInterpolatorConfig
+from ..filter.interpolation.PoseInterpolator import         PoseInterpolator
